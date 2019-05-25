f50b0218d4532613592a76bbc4d3fe72bf01c899
James Valles <james@jamesvalles.com>
2019-05-25 18:28:55 -0500

    Adding random letters and hashcode example

--- src/Thread1.java
+++ src/Thread1.java
@@ -1,13 +1,30 @@
+import com.sun.javafx.scene.traversal.Algorithm;
+import java.util.Random;
+
 public class Thread1 extends Thread {
 
   public void run() {
     int count = 0;
+
+
    for (int i = 0; i <=5; i++) {
+
+
+     String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+     Random random = new Random();
+     StringBuilder word = new StringBuilder();
+     for (int c = 0; c < 50; c++) {
+       word.append((alphabet.charAt(random.nextInt(alphabet.length()))));
+     }
+
+
       try {
         sleep(1000);
       } catch(Exception e){
 
       }
+     System.out.println("Hash: " + word.hashCode());
       count++;
       System.out.println("Timer B: "+ count);
     }
